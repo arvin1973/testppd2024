ec6d75169f59d17724a7c80cf82939a878781695
arvin1973 <rizki.arvin@gmail.com>
2024-10-07 13:37:03 +0700

    first commit

---
+++ application/logs/log-2024-10-07.php
@@ -0,0 +1,172 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-07 09:01:46 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 09:20:04 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 09:20:17 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:20:17 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:20:17 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:20:17 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:20:17 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:28:58 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 09:31:28 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 09:31:29 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 09:31:29 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 09:31:29 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 09:34:17 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:34:17 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:52:45 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:52:45 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:52:45 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:52:45 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:52:46 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:52:46 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:52:46 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:55:05 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:55:05 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:55:50 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:55:50 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:55:50 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:55:50 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:55:50 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 09:55:51 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 09:55:51 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:03:24 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:04:04 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:25 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:25 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:25 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:25 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:25 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:26 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:26 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:30 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:30 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:30 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:30 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:30 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:31 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:31 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:44 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:44 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:44 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:44 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:44 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:45 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:45 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:46 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:47 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:47 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:47 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:47 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:47 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:12:48 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:12:48 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:13:02 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:13:03 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:13:04 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:15:52 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:15:52 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:15:53 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:15:53 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:15:54 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:15:54 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:19:06 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:19:06 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:19:18 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:19:18 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:19:24 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:19:24 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:24:14 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:24:14 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:24:56 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:24:56 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:27:09 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:27:09 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 10:31:49 --> Severity: Parsing Error --> syntax error, unexpected '->' (T_OBJECT_OPERATOR) C:\xampp5\htdocs\ppd2024\application\controllers\Home.php 38
+ERROR - 2024-10-07 10:52:00 --> Severity: Notice --> Undefined property: CI_DB_mysqli_result::$userid C:\xampp5\htdocs\ppd2024\application\controllers\Home.php 36
+ERROR - 2024-10-07 10:53:10 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 10:55:42 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:55:42 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:55:42 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:55:42 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:55:42 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:57:13 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 10:59:00 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:01:41 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:01:41 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:01:41 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:01:41 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:01:41 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:03:04 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:08:30 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 11:09:29 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 11:10:16 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:10:16 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:10:17 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:15:24 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 11:20:05 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:21:58 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 11:22:21 --> 404 Page Not Found: Assets/js
+ERROR - 2024-10-07 11:22:21 --> 404 Page Not Found: Assets/js
+ERROR - 2024-10-07 11:22:46 --> 404 Page Not Found: Assets/js
+ERROR - 2024-10-07 11:22:46 --> 404 Page Not Found: Assets/js
+ERROR - 2024-10-07 11:36:56 --> 404 Page Not Found: Package/css
+ERROR - 2024-10-07 11:37:36 --> 404 Page Not Found: Assets/video_tutorial_3
+ERROR - 2024-10-07 11:38:14 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 11:38:14 --> 404 Page Not Found: Assets/libs
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 828
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 829
+ERROR - 2024-10-07 13:23:17 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at C:\xampp5\htdocs\ppd2024\application\libraries\third_party\PHPExcel\Shared\OLE\PPS\Root.php:279) C:\xampp5\htdocs\ppd2024\application\controllers\PPD1_status_penilaian_kota_daerah.php 830
